a45507b99d106db7625a4bbf3a8f3e0e5a90dad4
Valentin Mollina <ValentinMolina500>
2020-02-06 21:38:05 -0800

    Add memcpy func

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
+int memcpy()
+{
+    return 42;
+}
+
 int main(int argc, char **argv) 
 {
     int x = 9;
     cout << x << endl;
 
     string hello = "worlds";
-    
+
     return 0;
 }
